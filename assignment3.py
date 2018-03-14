ae9293a6681944df56118d442a31ed43d6db1394
SymmetricColors <36437005+SymmetricColors@users.noreply.github.com>
2018-03-14 13:39:14 +0330

    Create assignment3.py

---
+++ assignment3.py
@@ -0,0 +1,14 @@
+import socket
+
+ports=[21,22,25,80,110,9090]
+socket.setdefaulttimeout(5)
+f=open('host_names','r')
+host_names=f.read()
+host_names=host_names.split('\n')
+
+for x in host_names:
+    for y in ports:
+        try:
+           # you should write your codes in this block
+        except Exception as e:
+            print(e)
