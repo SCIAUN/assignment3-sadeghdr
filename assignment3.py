496d5387a6886ee0906973315a947e2aa2b3487d
sadeghdr <36894141+sadeghdr@users.noreply.github.com>
2018-03-28 19:41:37 +0430

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -9,6 +9,11 @@ host_names=host_names.split('\n')
 for x in host_names:
     for y in ports:
         try:
-           # you should write your codes in this block
+            s = socket.socket()
+            s.connect(x,y)
+            ans = s.recv(1024)
+            s.close()
+            print(ans)
+           
         except Exception as e:
             print(e)
